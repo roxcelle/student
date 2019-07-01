235cb2b6aa1085d42625fce0b9735e3b64d43556
roxcelle <1621151@uni-wuppertal.de>
2019-07-01 18:48:24 +0200

    main datei

---
+++ studentmain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "student.hpp"
+
+using namespace std;
+
+int main()
+{
+    Student a ( "Ana" , "Lind" , 1721165 );
+	Student b ( "Lucas" , "Schneider" , 1678421 );
+	Student c ( "Max" , "Müller" , 1429876 );
+    
+    return 0;
+
+
+}
+
